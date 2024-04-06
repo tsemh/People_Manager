7c648b494ad13ace91b3312f85da42b292df96b1
tsemh <tiago_barbosa-silva@hotmail.com>
2024-04-06 18:26:50 -0300

    Iniciando display

--- angularManager/src/app/app-routing.module.ts
+++ angularManager/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './view/display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'display', pathMatch: 'full' },
+  { path: 'display', component: DisplayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
